902209b08a18c63444e58acd327981e723f4733a
HowTo <alex.boca1992@gmail.com>
2018-05-03 22:21:44 +0300

    add seample config

--- config-sample.php
+++ config-sample.php
@@ -3,12 +3,12 @@ define('SITE_NAME', 'clotheshop');
 define('BASE_URL', 'http://localhost/' . SITE_NAME);
 define('ROOT', __DIR__ . '/');
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
+define('DB_USER', '');
 define('DB_PASS', '');
-define('DB_NAME', 'clotheshop');
-define('ADMIN_USERNAME', 'admin');
-define('ADMIN_PASSWORD', 'admin');
-define('MANAGER_EMAIL', 'admin@clotheshop.com');
+define('DB_NAME', '');
+define('ADMIN_USERNAME', '');
+define('ADMIN_PASSWORD', '');
+define('MANAGER_EMAIL', '');
 
 
 ?>﻿
